165b04b6f73bb53d71cc9b2d584f670c12155b19
CasualSpy <arnaudbegin@hotmail.com>
2020-09-22 20:39:09 -0400

    Fix to comply with specification

--- test.py
+++ test.py
@@ -2,23 +2,24 @@ import time
 continuer = True
 
 while continuer :
-    try :
-        entree = int(input("Entrez un nombre entier positif (0 pour terminer) : ")) # Prendre l'entrée de l'utilisateur et la convertir en int
+    entree_string = input("Entrez un nombre entier positif (0 pour terminer) : ") # Prendre l'entrée de l'utilisateur et la convertir en int
+    if entree_string.isnumeric():
+        entree = int(entree_string)
         if entree < 0 : # Rejeter les valeurs négatives
             print("Erreur, entrée non valide.")
         elif entree > 0: # Trouver les facteurs premiers
             start = time.perf_counter()
             valeur_depart = entree
-            facteurs = []
+            facteurs = ()
             i = 2 # Nombre à vérifier, commence à 2 et est incrémenté au fil de la recherche
             while i * i <= valeur_depart: # Vérifier les valeurs jusqu'à la racine carrée pour optimiser
                 if entree % i == 0 :
-                    facteurs.append(i)
+                    facteurs = facteurs + (i,)
                     entree = entree // i
                 else :
                     i = i + 1
             if entree > 1 :
-                facteurs.append(entree)
+                facteurs = facteurs + (entree,)
             print("La décomposition en facteurs premiers est : ", end='')
             if len(facteurs) > 0 :
                 print(facteurs[0], end='')
@@ -31,5 +32,5 @@ while continuer :
             print("Time to compute: {:0.4f} seconds".format(end - start))
         else : # Sortir de la boucle
             continuer = False
-    except ValueError: # Afficher une erreur parce que la valeur n'est pas un entier
+    else:
         print("Erreur, entrée non valide.")
